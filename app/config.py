20b3b3891a7ce5d172d32987857eaa143cb76157
Radoman-ra <rmaksim886@gmail.com>
2025-01-27 18:22:22 +0100

    Refactor configuration settings and update dependencies for Pydantic

--- app/config.py
+++ app/config.py
@@ -1,10 +1,9 @@
-from pydantic import BaseSettings
+from pydantic_settings import BaseSettings, SettingsConfigDict
 
 class Settings(BaseSettings):
     DATABASE_URL: str
-    SECRET_KEY: str = "your_secret_key"
+    SECRET_KEY: str = "mysecretkey"
 
-    class Config:
-        env_file = ".env"
+    model_config = SettingsConfigDict(env_file=".env")
 
 settings = Settings()
